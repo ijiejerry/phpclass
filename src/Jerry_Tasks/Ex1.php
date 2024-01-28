d55de3caac89b8f1c5115b00e218572f432f5b64
ijiejerry <143167220+ijiejerry@users.noreply.github.com>
2024-01-28 15:27:35 +0200

    firsts

---
+++ src/Jerry_Tasks/Ex1.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercise 1: Getting Started with PHP - Jerrys</title>
+
+    <h3>Ex3.1 Write PHP code to output the following message:
+    Hello World! my name is "Jerry"
+    </h3>
+
+    <?php
+    // use backslash to escape special characters like "
+    echo "Hello World! my name is \"Jerry\"";
+    ?>
+    <h3>3.2 Create a PHP variable named $title and assign it the
+        value "PHP
+        is interesting." Then, use this variable as the content within an (heading 4) element.</h3>
+   
+   
+</head>
+<body>
+    
+</body>
+</html>
